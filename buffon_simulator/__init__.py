5c5a48a13d1272eacc0eb1ed510b613720581d5b
손량 <loop.infinitely@gmail.com>
2019-05-11 19:17:01 +0900

    deleted a class

--- buffon_simulator/__init__.py
+++ buffon_simulator/__init__.py
@@ -4,9 +4,6 @@ import pyglet
 
 def run_simulation(count):
     """run the simulation & bash the homework"""
-    from buffon_simulator.simulator import Simulator
-    sim = Simulator(count)
-    sim.run_simulation()
     from buffon_simulator.app import App
     application = App()
     application.main_loop()
